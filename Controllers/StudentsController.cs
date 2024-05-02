c36769a9e1cae35714c31e11f306fcfbfa357ffa
mpnkab004 <mpnkab004@CA1-34>
2024-05-02 11:48:09 +0200

    Update the Delete page

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -64,12 +64,12 @@ namespace ContosoUniversity.Controllers
             try
             {
                 if (ModelState.IsValid)
-            {
-                _context.Add(student);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-           
+                {
+                    _context.Add(student);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+
             }
             catch (DbUpdateException /* ex */)
             {
@@ -132,7 +132,7 @@ namespace ContosoUniversity.Controllers
         }
 
         // GET: Students/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        public async Task<IActionResult> Delete(int? id, bool? saveChangesError = false)
         {
             if (id == null)
             {
@@ -140,10 +140,17 @@ namespace ContosoUniversity.Controllers
             }
 
             var student = await _context.Students
+                        .AsNoTracking()
                 .FirstOrDefaultAsync(m => m.ID == id);
             if (student == null)
             {
                 return NotFound();
+            }
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewData["ErrorMessage"] =
+                    "Delete failed. Try again, and if the problem persists " +
+                    "see your system administrator.";
             }
 
             return View(student);
